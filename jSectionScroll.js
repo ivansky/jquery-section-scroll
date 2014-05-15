0607f683bb34d6c5ca5aab988dad8805c34a6adb
Ivan <i@ivansky.ru>
2014-05-15 17:51:18 +0400

    Fix scroll down on last slide and renamed.

--- jSectionScroll.js
+++ jSectionScroll.js
@@ -12,7 +12,7 @@
 		resize: function(common){}
 	}
 	
-	$.fn.ivanSlider = function (options){
+	$.fn.jSectionScroll = function (options){
 		var positions_id = new Array(),
 			positions_top = [0,0],
 			offset_id = new Array(),
@@ -22,7 +22,6 @@
 			inwrapper = el.children().first().addClass('wrapInSlider').css({position:'absolute',width:'100%'}),
 			hellSection = $('<section></section>').appendTo(inwrapper).addClass('endOfAll'),
 			sections = inwrapper.children('section'),
-			current_index = 1,
 			lastAnimation = 0,
 			reloadSectionsTimeout = false,
 			windowHeight = 0,
@@ -31,7 +30,8 @@
 			statusScroll = true;
 		
 		var common = {
-			switchScroll:function(status){if(status) statusScroll = true; else statusScroll = false;}
+			switchScroll:function(status){if(status) statusScroll = true; else statusScroll = false;},
+			current: 1
 		};
 		
 		$.fn.slide = function(index, callback){
@@ -39,29 +39,20 @@
 			var __callback = callback || function(){};
 			var position = positions_id[index];
 			settings.beforeSlide(index, position.top);
-			/*
-			if(!settings.join(index)){
-				//el.slide(current_section);
-				
-				inwrapper.stop().css({top:(positions_id[current_section].top * -1)+'px'});
-				
-				return false;
-				
-			}else{
-			*/
+			
 				slideAction = true;
 				
 				inwrapper.animate({top:(position.top * -1)+'px'}, 400, 'swing', function(){
+					common.current = index;
 					__callback(); 
-					slideAction = false; 
+					slideAction = false;
 					settings.afterSlide(index, position.top, common);
+					
 					
 					var h = sections.filter(hellSection).first().position().top - windowHeight;
 					settings.slideEvent(position.top, position.top / h * 100);
 				});
-				//$(links).not(__current).removeClass('active');
-				//var __current = $(links).filter("[data-index='" + index +"']").addClass('active');
-				//$(links).not(__current).removeClass('active');
+				
 				$(links).each(function(){
 					var __index = parseInt($(this).data('index'));
 					if(index == __index) $(this).addClass('active');
@@ -81,11 +72,7 @@
 					if(is_active && !is_curr) $this.removeClass('active').fadeTo(400, 0.5);
 				});
 				
-				if(current_section){
-					//$(current_section)
-				}
 				return true;
-			//}
 		}
 		
 		var activeSlideIndex = 1;
@@ -159,7 +146,6 @@
 		function reload_sections(){
 			var __positions_id = positions_id;
 			sections.each(function(i, element){
-				//console.log(element);
 				__positions_id[i+1] = $(this).position();
 				if($(this).is(hellSection)) __positions_id[i+1].isHell = true;
 				positions_top = __positions_id[i+1].top;
@@ -183,10 +169,7 @@
 				sections.each(function(i, element){
 					var __index = i+1,
 						__thisY = $(this).position().top,
-						//__nextYDiff = $(window).height() - (__thisY - __current);
 						__nextYDiff = $(window).height() - (__thisY - __current);
-						//console.log('This 1: '+__nextYDiff);
-						//console.log('This 2: '+($(window).height() - __thisY + Math.abs($('.main').children().first().offset().top)));
 					if($(this).is(hellSection) && __nextYDiff > 120){
 						inwrapper.stop().animate({top:((__current - __nextYDiff)*-1)+'px'});
 						
@@ -195,7 +178,6 @@
 					
 						if(120 < __nextYDiff  && __nextYDiff <= 250){
 							__el.slide(__index);
-							//console.log(__nextYDiff);
 							return false;
 						}
 						
@@ -215,11 +197,9 @@
 					}else{
 						if(120 < __nextYDiff  && __nextYDiff <= 250){
 							__el.slide(__index);
-							console.log('CURRENT');
 							return false;
 						}
 						if(__nextYDiff > 250){
-							//console.log('MORE_BITCH');
 						 	//return true;
 						}
 					}
@@ -237,24 +217,15 @@
 					}else{
 						if(120 < __nextYDiff  && __nextYDiff <= 250){
 							__el.slide(__index - 1);
-							//console.log(__nextYDiff);
 							return false;
 						}
 						if(__nextYDiff > 250) return true;
 					}
 				});
-				//*/
 				
 				
 			}
-			/*
-			sections.each(function(i, element){
-				if($(this).is(hellSection)){
-					$(this).position().top;
-					
-				}
-			});
-			*/
+			
 			var h = sections.filter(hellSection).first().position().top - windowHeight;
 			settings.slideEvent(__current, __current / h * 100);
 			
@@ -270,23 +241,7 @@
 				$(this).children('.value').countTo();
 			});
 		}
-		/*
-		function check_active(current, new_position){
-			var current = Math.abs(inwrapper.offset().top),
-				position = positions_id[activeSlideIndex];
-			
-			if(current < new_position){
-				if(position.top > new_position) return join_slide(activeSlideIndex-1);
-				else return true;
-			}else{
-				var next_position = positions_id[activeSlideIndex+1];
-				console.log('238: next_position: '+next_position);
-				if((next_position.top - windowHeight) < current) return join_slide(activeSlideIndex+1);
-				else return true;
-			}
-			
-		}
-		*/
+		
 		function init_scroll(event, delta) {
 			var __current = inwrapper.offset().top;
 			if(!__current) __current = 0;
@@ -297,16 +252,9 @@
 			else if(delta > 0) __new = __current + settings.deltaScroll;
 			else return false;
 			
-			/*
-			if(!check_active(__current, __new)){
-				alert('NOPE');
-			}else{
-				//var __new = __current + (delta / 2);
-			*/
-				if(__new > 0) __new = 0;
-				inwrapper.css({top:__new+'px'});
-				check_position();
-			//}
+			if(__new > 0) __new = 0;
+			inwrapper.css({top:__new+'px'});
+			check_position();
 			
 		}
 		
@@ -318,33 +266,17 @@
 			if(!statusScroll) return false;
 			if(slideAction) return false;
 			
-			var inOffset = inwrapper.offset(),
-				currentTime = new Date().getTime();
+			var delta = event.originalEvent.wheelDelta || -event.originalEvent.detail,
+				inOffset = inwrapper.offset(),
+				inTop = Math.abs(inOffset.top),
+				currentTime = new Date().getTime(),
+				isToFirst = (delta > 0 && (inTop - sections.first().height() - settings.deltaScroll) < 0 ),
+				isToHell = (delta < 0 && (inTop + windowHeight) > positions_id[sections.length].top );
 			
-			var delta = event.originalEvent.wheelDelta || -event.originalEvent.detail;
-			
-			if(delta > 0){
-				var __firstHeight = $(sections[0]).height();
-				if(Math.abs(inOffset.top) - __firstHeight - settings.deltaScroll < 0){
-					inwrapper.css({top:(__firstHeight*-1)+'px'});
-					delta = 0;
-				}
-			}
-			if(delta < 0){
-				var __hellTop = positions_id[sections.length].top;
-				if((Math.abs(inOffset.top) + windowHeight) > __hellTop){
-					///*
-					window.clearTimeout(eventHellSlide);
-					eventHellSlide = window.setTimeout(function(){
-						inwrapper.stop().animate({top:((__hellTop - windowHeight)*-1)+'px'});
-					}, 100);
-					//*/
-					//inwrapper.stop().css({top:((__hellTop - windowHeight)*-1)+'px'});
-					delta = 0;
-				}
+			if(!isToFirst && !isToHell){
+				init_scroll(event, delta);	
 			}
 			
-			init_scroll(event, delta);
 		});
 		
 		reload_sizes();
